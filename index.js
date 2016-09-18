38ebb330022d276f22d2314f39d47832707cf1fa
Andrey Yatsyk <yatsyk@gmail.com>
2016-09-18 22:04:49 +0300

    fix

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
 'use strict';
 
-module.exports = function* clean(next){
-  this.finally = {
-    errorCallbacks: [],
-    finishCallbacks: [],
+module.exports = function koaFinally() {
+  rerturn function* (next) {
+    this.finally = {
+      errorCallbacks: [],
+      finishCallbacks: [],
 
-    onError(cb) {
-      this.errorCallbacks.push(cb);
-    },
+      onError(cb) {
+        this.errorCallbacks.push(cb);
+      },
 
-    onFinish(cb) {
-      this.finishCallbacks.push(cb);
+      onFinish(cb) {
+        this.finishCallbacks.push(cb);
+      }
     }
-  }
 
-  yield next;
+    yield* next;
 
-  this.finally.finishCallbacks.forEach(function(f) {
-    try {
-      c();
-    } catch (error) {
-      console.error(`clean middleware error ${error}`);
-    }
-  });
-  if (this.response.status > 399) {
-    console.info('calling error clear handlers');
-    this.finally.errorCallbacks.forEach(function(f) {
+    this.finally.finishCallbacks.forEach(function(f) {
       try {
         c();
       } catch (error) {
         console.error(`clean middleware error ${error}`);
       }
     });
+    if (this.response.status > 399) {
+      console.info('calling error clear handlers');
+      this.finally.errorCallbacks.forEach(function(f) {
+        try {
+          c();
+        } catch (error) {
+          console.error(`clean middleware error ${error}`);
+        }
+      });
+    }
   }
 }
